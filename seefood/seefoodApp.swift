680408ebc1bfc7b1a2d3748714d4f91144c49ab1
DavidWangwjh <davidwang.wjh@gmail.com>
2025-01-23 22:54:31 -0800

    Initial Commit

---
+++ seefood/seefoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  seefoodApp.swift
+//  seefood
+//
+//  Created by David Wang on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct seefoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
